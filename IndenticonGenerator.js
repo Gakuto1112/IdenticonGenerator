cabb283c7d6474d0572e15f388c11f5a1f4cc0f0
Gakuto1112 <gakuto131112@gmail.com>
2022-07-21 22:27:09 +0900

    キャンバスの初期化処理

--- IndenticonGenerator.js
+++ IndenticonGenerator.js
@@ -1,8 +1,15 @@
 const canvas = document.getElementById("icon_canvas");
-if(canvas.getContext) {
+let context;
+function init() {
+	context.fillStyle = "rgb(240, 240, 240)";
+	context.fillRect(0, 0, 420, 420);
+}
 
+if(canvas.getContext) {
+	context = canvas.getContext("2d");
+	init();
 }
 else {
 	document.getElementById("generator_area").classList.add("hidden");
-	document.getElementById("canvas_unavailable_message").classList.remove("hidden")
+	document.getElementById("canvas_unavailable_message").classList.remove("hidden");
 }
