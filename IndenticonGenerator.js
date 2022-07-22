92a6f2c33c0ba007f1d21b3a3a7fcbe2c57046c4
Gakuto1112 <gakuto131112@gmail.com>
2022-07-22 11:51:59 +0900

    キー文字列のハッシュ計算

--- IndenticonGenerator.js
+++ IndenticonGenerator.js
@@ -1,8 +1,24 @@
 const canvas = document.getElementById("icon_canvas");
 let context;
+
 function init() {
 	context.fillStyle = "rgb(240, 240, 240)";
 	context.fillRect(0, 0, 420, 420);
+}
+
+function generate(generateString) {
+	const reader = new FileReader();
+	reader.addEventListener("load", () => {
+		crypto.subtle.digest("SHA-1", reader.result).then((resolve) => {
+			const hash = [].map.call(new Uint8Array(resolve), x => ("00" + x.toString(16)).slice(-2)).join("");
+			console.log(hash);
+		});
+	}, {once: true});
+	reader.readAsArrayBuffer(new Blob([generateString]));
+}
+
+function generateButton() {
+	generate(document.getElementById("generate_key_string").value);
 }
 
 if(canvas.getContext) {
