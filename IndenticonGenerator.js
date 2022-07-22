ba211858ed1f3471d0ac62b650cc9f7ad056251e
Gakuto1112 <gakuto131112@gmail.com>
2022-07-22 12:12:30 +0900

    アイコンマークの生成

--- IndenticonGenerator.js
+++ IndenticonGenerator.js
@@ -10,14 +10,22 @@ function generate(generateString) {
 	const reader = new FileReader();
 	reader.addEventListener("load", () => {
 		crypto.subtle.digest("SHA-1", reader.result).then((resolve) => {
-			const hash = [].map.call(new Uint8Array(resolve), x => ("00" + x.toString(16)).slice(-2)).join("");
-			console.log(hash);
+			context.fillStyle = "black";
+			const hashString = [].map.call(new Uint8Array(resolve), x => ("00" + x.toString(16)).slice(-2)).join("");
+			const dotArray = Array.from(hashString.slice(0, 15));
+			dotArray.forEach((dotChar, index) => {
+				if(parseInt(dotChar, 16) % 2 == 0) {
+					if(index >= 5) context.fillRect(Math.floor(index / 5) * -70 + 175, index % 5 * 70 + 35, 70, 70);
+					context.fillRect(Math.floor(index / 5) * 70 + 175, index % 5 * 70 + 35, 70, 70);
+				}
+			});
 		});
 	}, {once: true});
 	reader.readAsArrayBuffer(new Blob([generateString]));
 }
 
 function generateButton() {
+	init()
 	generate(document.getElementById("generate_key_string").value);
 }
 
